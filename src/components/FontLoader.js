eb5ba48748e34b8a4b1a7354fff6c7307c5f4bcc
bren <bren@outlook.com>
2025-03-15 19:59:32 -0500

    Update FontLoader.js

--- src/components/FontLoader.js
+++ src/components/FontLoader.js
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSignature } from '../contexts/SignatureContext';
 
 export default function FontLoader() {
   const { adminSettings } = useSignature();
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   
   useEffect(() => {
     // Create a style element for font-face declarations
@@ -14,7 +15,7 @@ export default function FontLoader() {
     const fontFaces = `
       @font-face {
         font-family: 'CustomFont';
-        src: url('${adminSettings.fontUrls.regular}') format('woff2');
+        src: url('${adminSettings.fontUrls.regular}') format('ttf');
         font-weight: 400;
         font-style: normal;
         font-display: swap;
@@ -22,7 +23,7 @@ export default function FontLoader() {
       
       @font-face {
         font-family: 'CustomFont';
-        src: url('${adminSettings.fontUrls.medium}') format('woff2');
+        src: url('${adminSettings.fontUrls.medium}') format('ttf');
         font-weight: 500;
         font-style: normal;
         font-display: swap;
@@ -30,7 +31,7 @@ export default function FontLoader() {
       
       @font-face {
         font-family: 'CustomFont';
-        src: url('${adminSettings.fontUrls.bold}') format('woff2');
+        src: url('${adminSettings.fontUrls.bold}') format('ttf');
         font-weight: 700;
         font-style: normal;
         font-display: swap;
@@ -40,12 +41,71 @@ export default function FontLoader() {
     style.textContent = fontFaces;
     document.head.appendChild(style);
     
+    // Add preload links for the fonts to improve loading performance
+    adminSettings.fontUrls.regular && addPreloadLink(adminSettings.fontUrls.regular);
+    adminSettings.fontUrls.medium && addPreloadLink(adminSettings.fontUrls.medium);
+    adminSettings.fontUrls.bold && addPreloadLink(adminSettings.fontUrls.bold);
+
+    // Use the Font Loading API to detect when fonts are loaded
+    if ('fonts' in document) {
+      Promise.all([
+        document.fonts.load(`400 1em 'CustomFont'`),
+        document.fonts.load(`500 1em 'CustomFont'`),
+        document.fonts.load(`700 1em 'CustomFont'`)
+      ]).then(() => {
+        setFontsLoaded(true);
+        console.log('All fonts loaded successfully!');
+      }).catch(err => {
+        console.warn('Font loading issue:', err);
+        // Still set as loaded to not block the UI
+        setFontsLoaded(true);
+      });
+    } else {
+      // If Font Loading API is not available, assume fonts will load with CSS
+      setFontsLoaded(true);
+    }
+    
     // Clean up on unmount
     return () => {
       document.head.removeChild(style);
+      // Remove any preload links we added
+      document.querySelectorAll('link[data-font-preload="true"]').forEach(link => {
+        document.head.removeChild(link);
+      });
     };
   }, [adminSettings.fontUrls]);
   
-  // This component doesn't render anything
+  // Helper function to add preload links
+  const addPreloadLink = (url) => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.href = url;
+    link.as = 'font';
+    link.type = 'font/ttf';
+    link.crossOrigin = 'anonymous';
+    link.dataset.fontPreload = 'true';
+    document.head.appendChild(link);
+  };
+  
+  // This shows a loading indicator while fonts are loading
+  if (!fontsLoaded) {
+    return (
+      <div style={{ 
+        position: 'fixed', 
+        top: 0, 
+        left: 0, 
+        width: '100%', 
+        padding: '8px', 
+        backgroundColor: '#f0f9ff', 
+        color: '#0369a1',
+        textAlign: 'center',
+        fontSize: '14px',
+        zIndex: 1000
+      }}>
+        Loading brand fonts...
+      </div>
+    );
+  }
+  
   return null;
 }
