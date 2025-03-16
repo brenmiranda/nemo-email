4915d68643c508f21b82de47e4d392ac0651da69
bren <bren@outlook.com>
2025-03-15 19:31:35 -0500

    Create FontLoader.js

---
+++ src/components/FontLoader.js
@@ -0,0 +1,51 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useSignature } from '../contexts/SignatureContext';
+
+export default function FontLoader() {
+  const { adminSettings } = useSignature();
+  
+  useEffect(() => {
+    // Create a style element for font-face declarations
+    const style = document.createElement('style');
+    
+    // Generate @font-face rules from admin settings
+    const fontFaces = `
+      @font-face {
+        font-family: 'CustomFont';
+        src: url('${adminSettings.fontUrls.regular}') format('woff2');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }
+      
+      @font-face {
+        font-family: 'CustomFont';
+        src: url('${adminSettings.fontUrls.medium}') format('woff2');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+      }
+      
+      @font-face {
+        font-family: 'CustomFont';
+        src: url('${adminSettings.fontUrls.bold}') format('woff2');
+        font-weight: 700;
+        font-style: normal;
+        font-display: swap;
+      }
+    `;
+    
+    style.textContent = fontFaces;
+    document.head.appendChild(style);
+    
+    // Clean up on unmount
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, [adminSettings.fontUrls]);
+  
+  // This component doesn't render anything
+  return null;
+}
