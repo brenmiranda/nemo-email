e33b3bea46d0dea35ed5a2178e745a38e3550fc3
bren <bren@outlook.com>
2025-03-15 20:26:18 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         {/* Preload fonts to avoid flash of unstyled text */}
         <link
           rel="preload"
-          href="/fonts/RenaVF-pre-redraw.woff2"
+          href="https://cdn.jsdelivr.net/gh/brenmiranda/nemo-email@main/fonts/RenaVF-pre-redraw.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
